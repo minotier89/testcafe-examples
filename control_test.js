8831135c38ab583ee0d31aa4c45cf68cac606a2b
terumasa watanabe <terumasa.watanabe.biz@gmail.com>
2020-01-09 18:35:55 +0900

    Update async await pattern.

--- control_test.js
+++ control_test.js
@@ -1,23 +1,19 @@
 const Runner = require('testcafe');
 
-let testcafe = null;
-
-let runner;
-
 const args = process.argv;
 const browser = args[2];
 const screenshot_dir = args[3];
 
-Runner(null, '8000', '8001')
-    .then(tc => {
-        testcafe = tc;
-        runner = testcafe.createRunner();
-        return runner
+(async () => {
+    const testcafe = await Runner(null, '8000', '8001', null, true);
+    try {
+        await testcafe
+            .createRunner()
             .src(`tests/example_pages.js`)
             .browsers(browser)
             .screenshots(screenshot_dir)
             .run();
-    })
-    .finally(() => {
-        testcafe.close();
-    });
+    } finally {
+        await testcafe.close();
+    }
+})();
